1e7ea28c1cdfe10a37855105bd47f820bfc90a27
Troy Brown <brownts@troybrown.dev>
2024-07-13 13:54:30 -0400

    Bump version.

--- ada-ts-mode.el
+++ ada-ts-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Troy Brown <brownts@troybrown.dev>
 ;; Created: February 2023
-;; Version: 0.6.3
+;; Version: 0.7.0
 ;; Keywords: ada languages tree-sitter
 ;; URL: https://github.com/brownts/ada-ts-mode
 ;; Package-Requires: ((emacs "29.1"))
